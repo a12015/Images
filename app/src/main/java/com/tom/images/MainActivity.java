1695ba5ab76d6b26954cb710e833212b549da337
Hank Thomas <tom@snpy.org>
2015-04-27 20:41:49 +0800

    20150427-Image initial

--- app/src/main/java/com/tom/images/MainActivity.java
+++ app/src/main/java/com/tom/images/MainActivity.java
@@ -7,32 +7,79 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Cursor c = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 null, null, null, null);
-        while(c.moveToNext()){
-            int id = c.getInt(c.getColumnIndex(MediaStore.Images.Media._ID));
+
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.Media._ID));
             //String data = c.getString(c.getColumnIndex(MediaStore.Images.Thumbnails.DATA));
             //int imageId = c.getInt(c.getColumnIndex(MediaStore.Images.Thumbnails.IMAGE_ID));
-            Log.d("IMG", id+"/");
+            //int thumbId = c.getInt(c.getColumnIndex(MediaStore.Images.Media.MINI_THUMB_MAGIC));
+            String file = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            Log.d("IMG", id+"/"+file);
         }
-        /*
-        Cursor c = getContentResolver().query(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                null, null, null, null);
-        while(c.moveToNext()){
+        GridView grid = (GridView) findViewById(R.id.grid);
+
+
+
+
+//        grid.setAdapter(adapter);
+
+
+
+//        Cursor c = getContentResolver().query(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+//                null, null, null, null);
+        /*while(c.moveToNext()){
             int id = c.getInt(c.getColumnIndex(MediaStore.Images.Thumbnails._ID));
             String data = c.getString(c.getColumnIndex(MediaStore.Images.Thumbnails.DATA));
             int imageId = c.getInt(c.getColumnIndex(MediaStore.Images.Thumbnails.IMAGE_ID));
             Log.d("TH", id+"/"+data+"/"+imageId);
+        }*/
+    }
+
+    class ImageAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return cursor.getCount();
         }
-        */
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView==null){
+                ImageView img = new ImageView(MainActivity.this);
+                cursor.moveToPosition(position);
+                String file = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+//                img.set
+                convertView = img;
+            }
+            return convertView;
+        }
     }
 
 
